c0d5e89341b4141fada3415797bc3dddf1be2dbb
sergiobanderas <sergio.banderas@fpalanturing.es>
2026-08-26 16:47:13 +0200

    Next Level + Reload Level

--- Assets/Scripts/CollisionManager.cs
+++ Assets/Scripts/CollisionManager.cs
@@ -1,7 +1,10 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionManager : MonoBehaviour
 {
+
+    [SerializeField] private float reloadDelay = 1f;
 
     private void OnCollisionEnter(Collision collision)
     {
@@ -11,14 +14,33 @@ public class CollisionManager : MonoBehaviour
                 Debug.Log("Collided with landing pad");
                 break;
             case "LandingPad":
-                Debug.Log("Collided with landing pad");
+                GetComponent<PlayerMovement>().enabled = false;
+                Invoke(nameof(NextScene), reloadDelay);
                 break;
             case "Explosion":
-                Debug.Log("Collided with explosion");
+                GetComponent<PlayerMovement>().enabled = false;
+                Invoke(nameof(ReloadScene), reloadDelay);
                 break;
             default:
                 break;
         }
+        
+    }
+
+    private void ReloadScene()
+    {        
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    private void NextScene()
+    {        
+        if (SceneManager.GetActiveScene().buildIndex == SceneManager.sceneCountInBuildSettings - 1)
+        {
+            SceneManager.LoadScene(0);
+            return;
+        }
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         
     }
 }
